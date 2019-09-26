fc6cf43092d6da01a120fd6b3ce2f103db394b21
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-26 23:53:30 +0530

    [ swapnil ] Add. can check for two people

--- src/test/java/com/thoughtworks/address/AddressBookTest.java
+++ src/test/java/com/thoughtworks/address/AddressBookTest.java
@@ -19,4 +19,18 @@ public class AddressBookTest {
 
         Assertions.assertEquals(1, addressBook.get().size());
     }
+
+    @Test
+    public void givenTwoPerson_WhenRetrieve_ThenShouldReturnListWithTwoPerson() {
+        Address rajAddress = new Address("HSR layout","Bengluru","India","445001");
+        Person Raj = new Person("Raj","Bamb","8976542345",rajAddress);
+        Address apurvaAddress = new Address("HSR layout","Bengluru","India","445001");
+        Person Apurva = new Person("Raj","Bamb","8976542345",apurvaAddress);
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.add(Raj);
+        addressBook.add(Apurva);
+
+        Assertions.assertEquals(2, addressBook.get().size());
+    }
 }
